273e848cccb672fc8fef9349e7ddd067cad99cb7
Mohamed saeed <mmustafa@egypt.itgsolutions.com>
2018-12-27 14:32:43 +0200

    #Issue localizations

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import '../ui/movie_list.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      theme: ThemeData.dark(),
+      //theme: ThemeData.light(),
+      title: "Ewqewq",
       home: Scaffold(
-        body: MovieList(),
+
+        //appBar: AppBar(title: Text( "Hello")),
+        //drawer: Drawer(child: Container( padding: EdgeInsets.only(top: 50.0) ,child: Column(children: <Widget>[ Text("About Us") , Text("Quit")],)),),
+         body:  complexView() ),
+      // body : MovieList()  ),
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English
+        const Locale('ar', 'EG'), // Hebrew
+        // ... other locales the app supports
+      ],
+    );
+  }
+
+Widget complexView () {
+    return SingleChildScrollView(
+        child: SafeArea(
+          child: Column(children: <Widget>[
+            Image.network(
+              "https://github.com/flutter/website/blob/master/src/_includes/code/layout/lakes/images/lake.jpg?raw=true",
+              height: 300.0,
+              fit: BoxFit.fitWidth,
+            ),
+            BuildtitleSection(),
+            BuildButtonsSection() ,
+            buildDescription()
+          ]),
+        ));
+}
+  Container buildDescription() {
+    return Container( padding: EdgeInsets.symmetric(horizontal: 20.0 , vertical: 20.0), child: Text( "Swan Lake (Russian: Лебединое озеро Lebedinoye ozero), Op. 20"
+        ", is a ballet composed by Pyotr Ilyich Tchaikovsky in 1875–76. Despite its initial failure, it is now one of the most popular of all ballets.The scen"
+        "ario, initially in two acts, was fashioned from Russian and/or German folk tales[a] and tells the story of Odette, a princess turned into a swa"
+        " by an evil sorcerer's curse. The choreographer of the original production was Julius Reisinger (Václav Reisinger). The ballet wa"
+        "s premiered by the Bolshoi Ballet on 4 March [O.S. 20 February] 1877[1][2] at the Bolshoi Theatre in Moscow. Although it is pres"
+        "ented in many different versions, most ballet companies base their stagings both choreographically and musically on the 1895 "
+        "revival of Marius Petipa and Lev Ivanov, first staged for the Imperial Ballet on 15 January 1895, at the Mariinsky Theatre in St. "
+        "Petersburg. For this revival, Tchaikovsky's score was revised by the St. Petersburg Imperial Theatre's chief conductor and composer Riccardo Drigo.3"
+    ,style: TextStyle(color: Colors.blueGrey , fontSize: 14.0 ),
+    ));
+  }
+
+  Widget BuildTitleImgButton(IconData icon, String title) {
+    return MaterialButton(
+        child: Column(
+      children: <Widget>[Icon(icon), Text(title)],
+
+    ) ,onPressed: () {
+          print("hello");
+    },);
+  }
+
+  Widget BuildButtonsSection() {
+    return Container(
+        margin: EdgeInsets.only(top: 20.0),
+        child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              BuildTitleImgButton(Icons.call, "Call"),
+              BuildTitleImgButton(Icons.near_me, "Route"),
+              BuildTitleImgButton(Icons.share, "Share"),
+            ]));
+  }
+
+  Widget BuildtitleSection() {
+    return Container(
+      padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Swan Lake"),
+              Text(
+                "Berlin , Germany",
+                style: TextStyle(color: Colors.black45),
+              )
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Icon(Icons.star),
+              Text(
+                "41",
+                style: TextStyle(color: Colors.red),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
--- lib/ui/movie_list.dart
+++ lib/ui/movie_list.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import '../models/item_model.dart';
 import '../blocs/movie_bloc.dart';
 
-class movieDetail extends StatelessWidget {
+class movieDetail extends StatefulWidget {
   var link, title = "";
   movieDetail(String _link, String _title) {
     link = _link;
     title = _title;
   }
+
   @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MovieDetailsState() ;
+  }
+}
+class MovieDetailsState extends State <movieDetail> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
@@ -19,22 +26,30 @@ class movieDetail extends StatelessWidget {
           ),
         ),
         body: Padding(padding: EdgeInsets.only(top: 20.0) ,  child:  Center(child :
-        Column(children: <Widget>[
-          Image.network(
-            link,
-            fit: BoxFit.cover,
+        Column( mainAxisAlignment: MainAxisAlignment.spaceEvenly, mainAxisSize: MainAxisSize.max, children: <Widget>[
+          Container(
+            height: 500.0,
+            width: 500.0,
+            decoration: new  BoxDecoration( border: Border.all(width: 10.0, color: Colors.black38),
+//                    shape: BoxShape.circle,
+                borderRadius:
+                const BorderRadius.all(const Radius.circular(25.0)),
+                image: new DecorationImage(  fit: BoxFit.fill, image : NetworkImage(
+                    widget.link
+                ))),
+
           ),
           Padding(
               child: Text(
-                title,
+                widget.title,
                 softWrap: true,
                 textAlign: TextAlign.center,
               ),
               padding: EdgeInsets.symmetric(horizontal: 10.0))
         ], crossAxisAlignment: CrossAxisAlignment.center))));
   }
-}
 
+}
 class MovieList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -60,16 +75,19 @@ class MovieList extends StatelessWidget {
   Widget buildList(AsyncSnapshot<ItemModel> snapshot) {
     return GridView.builder(
         itemCount: snapshot.data.results.length,
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
           return GestureDetector(
-              child: Column(children: <Widget>[
-                Image.network(
-                  'https://image.tmdb.org/t/p/w185${snapshot.data.results[index].poster_path}',
-                  fit: BoxFit.cover,
-                ),
+                child: Column(children: <Widget>[
+                  Expanded(
+                    child: Image.network(
+                      'https://image.tmdb.org/t/p/w185${snapshot.data.results[index].poster_path}',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+ Center(child: Text('${snapshot.data.results[index].title}'))
+                ], crossAxisAlignment: CrossAxisAlignment.stretch ,),
 
-              ], crossAxisAlignment: CrossAxisAlignment.stretch ,),
               onTap: () {
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => movieDetail('https://image.tmdb.org/t/p/w185${snapshot.data.results[index].poster_path}' , '${snapshot.data.results[index].title}' )));
